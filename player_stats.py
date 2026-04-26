05fa043cd9f59d82ea76b25d7d2f02bb4b995fb9
jhon-nexter-dev <jhonnexterherrada00@gmail.com>
2026-04-26 10:46:57 +0800

    Add files via upload

---
+++ player_stats.py
@@ -0,0 +1,27 @@
+# Minecraft Player Data ni Jhon
+player_name = "Jhon Nexter"
+health = 20
+level = 1
+exp = 0
+diamonds = 0
+
+print ("=== PLAYER STATS ===")
+print ("Pangalan:", player_name)
+print ("Class: Future Software Developer")
+print ("Strand: TECH-PRO CSS")
+print ("Location: Polomolok")
+print ("Health:", health)
+print ("Level:", level)
+print ("EXP:", exp)
+print ("Diamonds:", diamonds)
+print ("====================")
+
+exp = exp + 100
+level = level + 1
+diamonds = diamonds + 64
+
+print ("LEVEL UP! Naka-first code ka!")
+print ("Bagong Level:", level)
+print ("Bagong EXP:", exp)
+print ("May", diamonds, "diamonds ka na!")
+print ("GG! Tuloy mo lang brad!")
