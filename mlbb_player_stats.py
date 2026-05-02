3f5d742bf951f1226af3051074aaa7ba53f8da06
jhon-nexter-dev <jhonnexterherrada00@gmail.com>
2026-05-02 22:02:52 +0800

    Add files via upload

---
+++ mlbb_player_stats.py
@@ -0,0 +1,25 @@
+print("=== MLBB PLAYER STATS ===")
+print("Gawa ni: Jhon Nexter - Day 5 Dev")
+print("Level 8 Project: The Stats Keeper")
+print("")
+
+# Dictionary = Stats ni Elnika
+elnika = {
+     "name": "Elnika",
+     "hero": "Claude",
+     "rank": "Top 1 Global",
+     "winrate": "78%",
+     "matches": 3200,
+     "skin": "Mecha Dragon"
+}
+
+print("TOP 1 GLOBAL PLAYER CARD:")
+print("-------------------------")
+print("Name:", elnika["name"])
+print("Main hero:", elnika["hero"])
+print("Current Rank:", elnika["rank"])
+print("Winrate:", elnika["winrate"])
+print("Total Matches:", elnika["matches"])
+print("Favorite Skin:", elnika["skin"])
+print("-------------------------")
+print("")
